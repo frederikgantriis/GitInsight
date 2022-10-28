8ca4d4b26e9a4bb8cb082fa71c97b286bcc1e8df
frmo24042 <frmo24042@gmail.com>
2022-10-28 14:09:27 +0100

    Added implementation of frequency Commit

---
+++ GitInsight.Tests/frequencyTest.cs
@@ -0,0 +1,10 @@
+namespace GitInsight.Tests;
+
+public class frequencyTest
+{
+    [Fact]
+    public void Given_no_Author_Then_Outputs_All_Commits_Per_Date()
+    {
+    
+    }
+}
--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,23 +1,56 @@
-﻿//namespace GitInsight;
+﻿namespace GitInsight;
 
 using LibGit2Sharp;
+using System.Globalization;
 
 public class Program
 {
-    public static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("lsjfakl");
+        //TODO: Implement method that Format output with author
+        getCommitsPerDay(@"../.git", "Frederik Gantriis Møller").ToList().ForEach(Console.WriteLine);
 
-        using (var repo = new Repository(@"..\.git"))
-        {
-            foreach (var commit in repo.Commits)
-            {
-              Console.WriteLine(commit);
-            }
-        }
+
+
     }
 
-    public void frequencyCommit() {
-        
+    
+    /// <summary>
+    /// This method returns a formatted string of commits per day, optionally only by a single author
+    /// </summary>
+    /// <param name="path">path to a local git repository</param>
+    /// <param name="author">optional: only get commits by author</param>
+    public static IEnumerable<string> getCommitsPerDay(string path, string author = "")
+    {
+        IDictionary<string, int> commitsPerDay = new Dictionary<string, int>();
+        using (var repo = new Repository(path))
+        {
+            var dateFormat = "dd-MM-yyyy";
+
+            foreach (var commit in repo.Commits)
+            {
+                var commitDateKey = commit.Author.When.Date.ToString(dateFormat, CultureInfo.InvariantCulture);
+
+                if (author != "" && author != commit.Author.Name)
+                {
+                    continue;
+                }
+
+                if (!commitsPerDay.ContainsKey(commitDateKey))
+                {
+                    commitsPerDay[commitDateKey] = 1;
+                }
+                else
+                {
+                    commitsPerDay[commitDateKey]++;
+                }
+
+            }
+
+            foreach (var commitDate in commitsPerDay)
+            {
+                yield return $"{commitDate.Key} {commitDate.Value}";
+            }
+        }
     }
 }
