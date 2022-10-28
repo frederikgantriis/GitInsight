35dd3a4b2df588116269ac9555f63e1f819274fb
frmo24042 <frmo24042@gmail.com>
2022-10-28 13:07:54 +0100

    Added things that doesn't work. You're welcome.
    Co-authored-by: Silas Bue Handgaard <silasbue@users.noreply.github.com>
    Co-authored-by: Magnus Brun Larsen <magnusblarsen@users.noreply.github.com>

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//namespace GitInsight;
+
+using LibGit2Sharp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("lsjfakl");
+
+        using (var repo = new Repository(@"..\.git"))
+        {
+            foreach (var commit in repo.Commits)
+            {
+              Console.WriteLine(commit);
+            }
+        }
+    }
+
+    public void frequencyCommit() {
+        
+    }
+}
