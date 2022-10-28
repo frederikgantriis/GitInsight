a25ab4cde9a5ffb396a06dbf3ef4b9bbaf3162d0
Frederik Gantriis Møller <93861344+frmo24042@users.noreply.github.com>
2022-10-28 14:11:21 +0100

    Commit frequency (#4)
    * Added things that doesn't work. You're welcome.
    
    Co-authored-by: Silas Bue Handgaard <silasbue@users.noreply.github.com>
    Co-authored-by: Magnus Brun Larsen <magnusblarsen@users.noreply.github.com>
    
    * Added implementation of frequency Commit
    
    * Added todo in test
    
    Co-authored-by: Silas Bue Handgaard <silasbue@users.noreply.github.com>
    Co-authored-by: Magnus Brun Larsen <magnusblarsen@users.noreply.github.com>

---
+++ GitInsight.Tests/frequencyTest.cs
@@ -0,0 +1,12 @@
+namespace GitInsight.Tests;
+
+public class frequencyTest
+{
+
+    //TODO: Implement Tests for Frequency Commits
+    [Fact]
+    public void Given_no_Author_Then_Outputs_All_Commits_Per_Date()
+    {
+    
+    }
+}
--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GitInsight;
+
+using LibGit2Sharp;
+using System.Globalization;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //TODO: Implement method that Format output with author
+        getCommitsPerDay(@"../.git", "Frederik Gantriis Møller").ToList().ForEach(Console.WriteLine);
+
+
+
+    }
+
+    
+    /// <summary>
+    /// This method returns a formatted string of commits per day, optionally only by a single author
+    /// </summary>
+    /// <param name="path">path to a local git repository</param>
+    /// <param name="author">optional: only get commits by author</param>
+    public static IEnumerable<string> getCommitsPerDay(string path, string author = "")
+    {
+        IDictionary<string, int> commitsPerDay = new Dictionary<string, int>();
+        using (var repo = new Repository(path))
+        {
+            var dateFormat = "dd-MM-yyyy";
+
+            foreach (var commit in repo.Commits)
+            {
+                var commitDateKey = commit.Author.When.Date.ToString(dateFormat, CultureInfo.InvariantCulture);
+
+                if (author != "" && author != commit.Author.Name)
+                {
+                    continue;
+                }
+
+                if (!commitsPerDay.ContainsKey(commitDateKey))
+                {
+                    commitsPerDay[commitDateKey] = 1;
+                }
+                else
+                {
+                    commitsPerDay[commitDateKey]++;
+                }
+
+            }
+
+            foreach (var commitDate in commitsPerDay)
+            {
+                yield return $"{commitDate.Key} {commitDate.Value}";
+            }
+        }
+    }
+}
