aed3e03c4f240a837572c5835779a36f4b285af1
Magnus Brun Larsen <magnusbrunlarsen00@gmail.com>
2022-11-01 16:18:28 +0100

    Implement command line parsing

--- GitInsight/Program.cs
+++ GitInsight/Program.cs
@@ -2,12 +2,31 @@
 
 using LibGit2Sharp;
 using System.Globalization;
+using CommandLine;
 
 public class Program
 {
-  private static void Main(string[] args)
-  {
-    //TODO: Implement method that Format output with author
-    CommitTracker.getCommitsPerDay(@"../.git", "Frederik Gantriis Møller").ToList().ForEach(Console.WriteLine);
-  }
+    public class Options
+    {
+        [Option('a', "author", Required = false, HelpText = "Display commits categorized by author.")]
+        public bool Author { get; set; }
+
+    }
+    private static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+          .WithParsed<Options>(o =>
+          {
+              if (o.Author)
+              {
+                  //TODO: Implement method that Format output with author
+                  Console.WriteLine($"Author mode.");
+              }
+              else
+              {
+                  Console.WriteLine("Commit frequency mode");
+                  CommitTracker.getCommitsPerDay(@"../.git", "Frederik Gantriis Møller").ToList().ForEach(Console.WriteLine);
+              }
+          });
+    }
 }
