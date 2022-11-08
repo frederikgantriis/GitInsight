b5d950bef834398554dc12dc893d20c15cb81d38
Silas Bue Handgaard <silas@felskov-aps.dk>
2022-11-08 10:57:15 +0100

    Initial setup of postgresql database

---
+++ GitInsight/Models/GitInsightContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace GitInsight.Models;
+
+public partial class GitInsightContext : DbContext
+{
+  public GitInsightContext()
+  {
+  }
+
+  public GitInsightContext(DbContextOptions<GitInsightContext> options)
+      : base(options)
+  {
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+    var connectionString = configuration.GetConnectionString("GitInsight");
+
+    optionsBuilder.UseNpgsql(connectionString);
+  }
+
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    OnModelCreatingPartial(modelBuilder);
+  }
+
+  partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
