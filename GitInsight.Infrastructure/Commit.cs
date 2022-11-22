37745efbde45cf79fb0b7955384100a588866182
Silas Bue Handgaard <silas@felskov-aps.dk>
2022-11-22 13:17:18 +0100

    move entities to infrastructure

---
+++ GitInsight.Infrastructure/Commit.cs
@@ -0,0 +1,9 @@
+namespace GitInsight.Infrastructure;
+
+public class Commit
+{
+  public int Id { get; set; }
+  public int User_Id { get; set; }
+  public int Repo_Id { get; set; }
+  public DateTime date { get; set; }
+}
