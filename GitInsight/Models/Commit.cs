33bccc9f08ee19913ec53a37fc9bb945b830a9b0
Silas Bue Handgaard <48789220+silasbue@users.noreply.github.com>
2022-11-25 13:13:53 +0100

    removed models and packages (#52)

--- GitInsight/Models/Commit.cs
+++
@@ -1,8 +0,0 @@
-namespace GitInsight.Models;
-
-public class Commit {
-  public int Id {get; set;}
-  public int User_Id {get; set;}
-  public int Repo_Id{get; set;}
-  public DateTime date{get; set;}
-}
